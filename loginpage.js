8e89d7b5b8366d97ebdd7907fdb5caa9924d64dc
DDing-J <myungjae6596@gmail.com>
2024-08-20 11:35:16 +0900

    Add files via upload
    처음 올리는 로그인 페이지 입니다

---
+++ loginpage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Login.css'; 
+
+function Login() {
+    return (
+        <div className="login-container">
+            <input
+                type="text"
+                className="login-input"
+                placeholder="ID또는 전화번호"
+            />
+            <input
+                type="password"
+                className="login-input"
+                placeholder="비밀번호"
+            />
+            <button className="login-button">로그인</button>
+        </div>
+    );
+}
+
+export default Login;
