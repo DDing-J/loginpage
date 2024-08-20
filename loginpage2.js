612c43b8af8fa8407f7bb66035c40d5c5f99dbe6
DDing-J <myungjae6596@gmail.com>
2024-08-20 11:40:54 +0900

    login page 다음 화면 연결 js입니다
    세번째 로그인 페이지 js입니다

---
+++ loginpage2.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './Login';
+
+function App() {
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
